b9ce52644100ca8c12888f4a242c03615ea0f88f
robruzzo <robruzzo@gmail.com>
2021-02-18 16:57:00 -0500

    Updated to test all current features of GPS_PAM_7Q

--- gps_class_test.py
+++ gps_class_test.py
@@ -8,10 +8,11 @@ gps = PAM_7Q_Interface('2')
 
 #These can be used to test to make sure all the messages are received and printed
 
-'''
+
 RMC=gps.getRMC_Data()
 gps.printRMC(RMC)
-    
+
+  
 VTG=gps.getVTG_Data()
 gps.printVTG(VTG)
    
@@ -23,7 +24,7 @@ gps.printGSA(GSA)
 
 GLL=gps.getGLL_Data()
 gps.printGLL(GLL)
-'''
+
 
 #This part will grab all of the available messages and dump them all to the screen raw
 
@@ -36,5 +37,5 @@ while x<1:
         print(messages[i].strip().split(','))
         print("\n")    
     x+=1
-    
+   
  
