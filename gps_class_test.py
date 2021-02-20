88e25d5333bf5331de93e186116ac0512deb9a43
robruzzo <robruzzo@gmail.com>
2021-02-20 13:44:09 -0500

    Updated to test all current features of GPS_PAM_7Q

--- gps_class_test.py
+++ gps_class_test.py
@@ -6,36 +6,51 @@ x=0
 #Create an instance of the PAM_7Q_Interface object
 gps = PAM_7Q_Interface('2')
 
+'''
+Comment and uncomment each section to test functions accordingly
+'''
+
 #These can be used to test to make sure all the messages are received and printed
 
-
-RMC=gps.getRMC_Data()
+'''
+RMC=gps.getRMCData()
 gps.printRMC(RMC)
 
   
-VTG=gps.getVTG_Data()
+VTG=gps.getVTGData()
 gps.printVTG(VTG)
    
-GGA = gps.getGGA_Data()
+GGA = gps.getGGAData()
 gps.printGGA(GGA)
 
-GSA = gps.getGSA_Data()
+GSA = gps.getGSAData()
 gps.printGSA(GSA)
 
-GLL=gps.getGLL_Data()
+GLL=gps.getGLLData()
 gps.printGLL(GLL)
-
+'''
 
 #This part will grab all of the available messages and dump them all to the screen raw
-
+'''
 messages =[]
 
 while x<1:
-    messages = gps.get_message()
+    messages = gps.getMessage()
 
     for i in range(len(messages)):
         print(messages[i].strip().split(','))
         print("\n")    
     x+=1
-   
- 
+'''
+
+
+#This part will test the getAllData function
+RMC, VTG, GGA, GSA, GLL = gps.getAllData()
+
+gps.printRMC(RMC)
+gps.printVTG(VTG)
+gps.printGGA(GGA)
+gps.printGSA(GSA)
+gps.printGLL(GLL)
+
+
