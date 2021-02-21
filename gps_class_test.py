4b232f7c57e0285bc1133cda4062eb185dcb599a
robruzzo <robruzzo@gmail.com>
2021-02-20 20:35:41 -0500

    Added a line to test GPS_PAM_7Q_p3.py

--- gps_class_test.py
+++ gps_class_test.py
@@ -1,4 +1,9 @@
-from GPS_PAM_7Q import PAM_7Q_Interface
+'''
+Use GPS_PAM_7Q for python 2.7
+Use GPS_PAM_7Q_P3 for python 3
+'''
+from GPS_PAM_7Q_P3 import PAM_7Q_Interface
+#from GPS_PAM_7Q import PAM_7Q_Interface
 from time import sleep
 
 x=0
@@ -29,22 +34,23 @@ gps.printGSA(GSA)
 GLL=gps.getGLLData()
 gps.printGLL(GLL)
 '''
-
-#This part will grab all of the available messages and dump them all to the screen raw
 '''
+#This part will grab all of the available messages and dump them all to the screen raw
+
 messages =[]
 
-while x<1:
+while x<10:
     messages = gps.getMessage()
 
     for i in range(len(messages)):
         print(messages[i].strip().split(','))
         print("\n")    
     x+=1
+
+
 '''
-
-
 #This part will test the getAllData function
+
 RMC, VTG, GGA, GSA, GLL = gps.getAllData()
 
 gps.printRMC(RMC)
