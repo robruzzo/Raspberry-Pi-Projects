5b535d4a076f8e7e9a031a94afaf1c4a7d55f55c
robruzzo <robruzzo@gmail.com>
2021-02-24 20:29:40 -0500

    Updated with PMTK Strings Initial

--- SIM33EAU.py
+++ SIM33EAU.py
@@ -29,10 +29,10 @@ posMode (GLL, RMC, VTG, GNS) : N = No Fix, E = Estimated/Dead Reckoning Fix, A= 
 '''
 
 class SIM33EAU:
-    def __init__(self, port):
+    def __init__(self, port,baud):
         #Define setup parameters
         self.port = port
-        self.baud = 9600
+        self.baud = baud
         self.bytesize = serial.EIGHTBITS
         #Data Variables
         self.byte=''           #Byte to determine if sentences are being transmitted
@@ -60,7 +60,6 @@ class SIM33EAU:
                             break
                         else:
                             messages.append(message)
-                    
         return messages
         
     def getRMCData(self, messages=None):
@@ -233,9 +232,7 @@ class SIM33EAU:
         
         return RMC, VTG, GGA, GSA, GLL
         
-        
-        
-   #Printing Functions
+    #Printing Functions
     
     def printPrefix(self, Suffix):
         if Suffix=="GP":
@@ -322,4 +319,94 @@ class SIM33EAU:
         print("PosMode: "+GLL["PosMode"])
         print("Checksum: "+GLL["Checksum"]+"\n")
         
-        
+    def createBaudRatePacket(self,baudrate):
+        baudrate = str(baudrate)
+        packet="PMTK251"+","+baudrate
+        check=self.checksum(packet)[2:]
+        if len(check)==1:
+            check="0"+check
+        output="$"+packet+"*"+check+"\r\n"
+        output=output.upper()
+        output=str.encode(output)
+        return output
+    
+    def createTestPacket(self):
+        packet="PMTK000"
+        check=checksum(packet)[2:]
+        if len(check)==1:
+            check="0"+check
+        output="$"+packet+"*"+check+"\r\n"
+        output=output.upper()
+        output=str.encode(output)
+        return output
+
+    def createNMEAOutputPacket(self,GPGLL=0, GPRMC=1, GPVTG=0, GPGGA=1, GPGSA=0, GPGSV=0, GPGRS=0, GPGST=0,GPZDA=0, MCHN=0, DTM=0,Default=False):
+        if Default:
+            packet="PMTK314,-1"
+        else:
+            packet="PMTK314"+","+str(GPGLL)+","+str(GPRMC)+","+str(GPVTG)+","+str(GPGGA)+","+str(GPGSA)+","+str(GPGSV)+","+str(GPGRS)+","+str(GPGST)+",0,0,0,0,0,0,0,0,0,"+str(GPZDA)+","+str(MCHN)+","+str(DTM)
+        check=checksum(packet)[2:]
+        if len(check)==1:
+            check="0"+check
+        output="$"+packet+"*"+check+"\r\n"
+        output=output.upper()
+        output=str.encode(output)
+        return output
+
+
+    def checksum(self,packet):
+        checksum = 0
+        for el in packet:
+            checksum ^= ord(el)
+        return hex(checksum)
+    
+    def sendPacket(self,packet):
+        self.ser.flush()
+        self.ser.write(packet)
+        self.ser.flush()
+        print("Sent output, awaiting response...")
+
+    def awaitResponse(self,i):
+        x=0
+        responded = False
+        #ser.flushInput()
+        while x<i:
+            response=self.ser.readline()
+            print(response)
+            if len(response)>1:
+                response=response.decode()
+                if response.strip().split(',')[0][1:5]=="PMTK":
+                    break
+            x+=1
+        if len(response)>1:
+            if response.strip().split(',')[0][1:8]=="PMTK001":
+                command=response.strip().split(',')[1]
+                response_flag=response.strip().split(',')[2][0]
+                if response_flag=="0":
+                    response_text="Invalid command or packet"
+                if response_flag=="1":
+                    response_text="Unsupported command or packet type"
+                if response_flag=="2":
+                    response_text="Valid command and packet, but action failed"
+                if response_flag=="3":
+                    response_text="Valid command and packet, action succeeded"
+                print("Command: "+command+"  Response: "+response_text)
+        else:
+            print("ERROR: FAILED TO ACKNOWLEDGE")
+ 
+    def grabAndPrintData(self,num):
+        x=0
+        self.ser.flushInput()
+        while x < num:
+            response=self.ser.readline()
+            if len(response)>1:
+                response=response.decode()
+                if response.strip().split(',')[0][0]=="$":
+                    print(response)
+            x+=1
+            
+    def changeBaudRate(self,baudrate):
+        self.baud=baudrate
+        baudRatePacket=self.createBaudRatePacket(self.baud)
+        self.sendPacket(baudRatePacket)
+        self.ser.baudrate=self.baud
